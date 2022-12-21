79f4ad17c0e7b58af63bdde4ed414bc90b96e205
GroypySpoopyBoi <olegwolf360@gmail.com>
2022-12-21 05:47:43 +0300

    dsadfa

--- MainActivity.java
+++ MainActivity.java
@@ -17,7 +17,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     CountDownTimer timer;
-    Button addview;
+    Button add;
     Button button1;
     Button button2;
     Button button3;
